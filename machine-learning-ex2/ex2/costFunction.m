86f0d87cdea380f75f823b5ae4d7fb2dfceabca4
zxh <zxh_sea@outlook.com>
2018-02-26 11:24:58 +0800

    s2

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -4,6 +4,11 @@ function [J, grad] = costFunction(theta, X, y)
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
 
+%COSTFUNCTION为逻辑回归计算成本和梯度
+%J = COSTFUNCTION（theta，X，y）计算使用theta的成本
+%用于逻辑回归的％参数和成本的梯度
+%w.r.t. 到参数。
+
 % Initialize some useful values
 m = length(y); % number of training examples
 
@@ -16,16 +21,19 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+%指令：计算特定选项的成本。
+%您应该将J设置为成本。
+%计算偏导数并将grad设置为部分
+%成本w.r.t的％衍生物 theta中的每个参数
+
 %
 % Note: grad should have the same dimensions as theta
 %
+%J = (1./m)*sum(-y*log(sigmoid(theta'*X'))-(1-y)log(1-sigmoid(theta'*X')))
+J = (log(sigmoid(theta'*X')) * y + log(1-sigmoid(theta'*x'))*(1-y))/(-m);
 
 
-
-
-
-
-
+grad = (X'*(sigmoid(X*theta)-y))/m;
 
 % =============================================================
 
--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -3,13 +3,15 @@ function g = sigmoid(z)
 %   g = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
+%您需要正确返回以下变量
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
+%指令：计算每个z值的S形（z可以是矩阵，
+%向量或标量）。
+g = 1./(ones(size(z))+exp(-z))%‘点除’ 表示 1 除以矩阵(向量)中的每一个元素
 
 
 

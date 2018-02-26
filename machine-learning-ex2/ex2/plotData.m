75a9d75acdea658ea49aa1493ab9da8e26f32387
zxh <zxh_sea@outlook.com>
2018-02-26 10:05:34 +0800

    s1

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -4,6 +4,10 @@ function plotData(X, y)
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
+%PLOTDATA将数据点X和y绘制成新图
+%PLOTDATA（x，y）用+表示正数示例的数据点
+%和o为负面的例子。 假设X是M×2矩阵。
+%创建新图
 figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
@@ -11,8 +15,14 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
-
-
+%指示：绘制a的正面和负面的例子
+%2D图，使用选项'k +'作为正数
+%例子和'ko'为负面例子。
+pos = find(y==1);
+neg = find(y==0);
+plot(X(pos,1),X(pos,2),'k+','LineWidth',2,'MarkerSize',7);
+%x中对应y等于1的第一列和第二列
+plot(X(neg,1),X(neg,2),'ko','MarkerFaceColor','y','MarkerSize',7);
 
 
 

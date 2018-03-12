198c83c539a70d8815ace45feb0125847f040f67
zxh_s <zxh_sea@outlook.com>
2018-03-12 18:29:12 +0800

    wrote a readme file

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -32,8 +32,10 @@ m = size(X, 1);
          
 % You need to return the following variables correctly 
 J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
+Theta1_grad = zeros(size(Theta1));% Theta1_grad is a 25*401 matrix
+Theta2_grad = zeros(size(Theta2));% Theta2_grad is a 10*26 matrix
+% 当每一层的残差计算好之后，就可以更新 Δ(delta) 矩阵了，Δ(delta) 矩阵与 参数矩阵有相同的维数，初始时Δ(delta) 矩阵中的元素全为0.
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
@@ -44,11 +46,39 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %第1部分：前馈神经网络，并返回变量J的成本。实现第1部分后，可以通过验证ex4.m中计算的成本来验证您的成本函数计算是否正确
+a1 = [one(m,1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [one(m,1) a2];
+z3 = a2 * Theta2';
+h = sigmoid(z3);
+
+for i = 1:m
+	yk(i,y(i)) = 1;
+end
+J = (1/m)*sum(sum(((-yk).*log(h) - ( 1- yk).*log(1-h)))
+
+Theta1_new=Theta1(:,2:size(Theta1,2));  
+Theta2_new=Theta2(:,2:size(Theta2,2));  
+J=J+lambda/2/m*(Theta1_new(:)'*Theta1_new(:)+Theta2_new(:)'*Theta2_new(:));  
+
+
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
 %         Theta2_grad, respectively. After implementing Part 2, you can check
 %         that your implementation is correct by running checkNNGradients
+
+
+
+
+
+
+
+
+
+
 %第2部分：实现反向传播算法来计算梯度Theta1_grad和Theta2_grad。 
 %您应该分别返回关于Theta1_grad和Theta2_grad中的Theta1和Theta2的成本函数的偏导数。
 % 在实现第2部分之后，您可以通过运行checkNNGradients来检查您的实现是否正确
--- machine-learning-ex4/ex4/randInitializeWeights.m
+++ machine-learning-ex4/ex4/randInitializeWeights.m
@@ -1,13 +1,15 @@
 function W = randInitializeWeights(L_in, L_out)
 %RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
 %incoming connections and L_out outgoing connections
+% RANDINITIALIZEWEIGHTS使用L_in传入连接和L_out传出连接随机初始化图层的权重
+
 %   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
 %   of a layer with L_in incoming connections and L_out outgoing 
 %   connections. 
-%
+%W = RANDINITIALIZEWEIGHTS（L_in，L_out）随机地使用L_in传入连接和L_out传出连接初始化图层的权重。
 %   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
 %   the first column of W handles the "bias" terms
-%
+%请注意，由于W的第一列处理“bias”项，因此W应设置为大小矩阵（L_out，1 + L_in）
 
 % You need to return the following variables correctly 
 W = zeros(L_out, 1 + L_in);
@@ -15,11 +17,11 @@ W = zeros(L_out, 1 + L_in);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Initialize W randomly so that we break the symmetry while
 %               training the neural network.
-%
+%说明：随机初始化W，以便在训练神经网络时打破对称性。
 % Note: The first column of W corresponds to the parameters for the bias unit
-%
-
-
+%注意：W的第一列对应于偏置单元的参数
+epsilon_init = 0.12;  
+    W = rand(L_out, 1 + L_in) * 2 * epsilon_init-epsilon_init;  
 
 
 
